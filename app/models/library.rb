cc20a59142dbff1639147ecd167d751b4f317e93
Ravi Bhim <ravi.bhim@yahoo.com>
2013-11-14 19:11:40 +0530

    om - good start

---
+++ app/models/library.rb
@@ -0,0 +1,4 @@
+class Library
+  include Mongoid::Document
+  field :title, type: String
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JaffaChat::Application.config.secret_token = '0836f0f043d84733e19f6935a66e1d03e94b89be286a2298e2b882d3ebae9f0dc49e97611120506412da1cb1b03653255404386ad5673473322c82eb9ced82a2'
