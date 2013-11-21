54b7c62f7e41ce7520e8eb75d3e8019629c0f9d3
Pradeep G <itsprdp@gmail.com>
2013-11-21 12:29:18 +0530

    Chef for vagrant-aws

---
+++ jaffa-chef/cookbooks/jaffa/recipes/default.rb
@@ -0,0 +1,32 @@
+#
+# Cookbook Name:: logg-chef
+# Recipe:: default
+#
+# Copyright (C) 2013 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'apt'
+
+%w[curl gawk libreadline6-dev zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 autoconf libgdbm-dev libncurses5-dev automake libtool bison pkg-config libffi-dev build-essential nodejs].each do |pkg|
+  package pkg
+end
+
+user "deploy" do
+  comment "Deploy User"  
+  gid "sudo"
+  supports :manage_home => true # needed to actually create home dir
+  home "/home/deploy"
+  shell "/bin/bash"
+  password '$1$VT/F5P.y$GuSLRwrDSiT2rArmstNQO1' 
+end
+
+
+package "openjdk-6-jdk"
+
+include_recipe "nginx"
+include_recipe "git"
+include_recipe "mongodb::10gen_repo"
+include_recipe "mongodb"
+
